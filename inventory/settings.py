4ce5fa63957df043f7cb45876defe663f06c51a3
Nam3Mc <moralesdreiser5@gmail.com>
2025-10-20 09:18:15 -0500

    environment variables

--- inventory/settings.py
+++ inventory/settings.py
@@ -98,23 +98,23 @@ WSGI_APPLICATION = 'inventory.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
     # 'default': {
-        # 'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': config('NAME_DB'),
-        # 'USER': config('USER_BD'),
-        # 'PASSWORD': config('PASSWORD_DB'),
-        # 'HOST': config('HOST'),
-        # 'PORT': config('PORT'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     # }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
